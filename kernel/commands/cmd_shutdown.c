f1e8109520407cb52693a3893dc95ae327159f36
anileneserden <109215783+anileneserden@users.noreply.github.com>
2026-03-10 18:01:08 +0300

    shell: support shutdown --now

--- kernel/commands/cmd_shutdown.c
+++ kernel/commands/cmd_shutdown.c
@@ -1,10 +1,21 @@
 #include <ui/power_screen.h>
 #include <lib/commands.h>
+#include <lib/string.h>
+#include <kernel/power.h>
 
 void cmd_shutdown(int argc, char** argv)
 {
-    (void)argc; (void)argv;
+    if (argc >= 2) {
+        if (!strcmp(argv[1], "--now") || !strcmp(argv[1], "now")) {
+            power_shutdown();
+            return;
+        }
+
+        commands_puts("Kullanim: shutdown [--now|now]\n");
+        return;
+    }
+
     ui_power_screen_shutdown(3);
 }
 
-REGISTER_COMMAND(shutdown, cmd_shutdown, "Sistemi güvenli bir şekilde kapatır");
+REGISTER_COMMAND(shutdown, cmd_shutdown, "Sistemi guvenli bir sekilde kapatir");
