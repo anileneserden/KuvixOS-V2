a72a63295705c55ab17b0cd22bf9d79f6268a9f7
Anıl Enes Erden <github.anileneserden@gmail.com>
2026-06-11 13:46:19 +0300

    feat(de): add rectangle and text drawing bridges to de_api
    - Include draw_rect and draw_text function pointers in the DE_API struct.
    - Implement kernel_draw_rect and kernel_draw_text wrappers to fix parameter ordering.
    - Update desktop loader file path to /sys/de/desktop.kde.

--- include/kernel/drivers/video/de_api.h
+++ include/kernel/drivers/video/de_api.h
@@ -4,8 +4,13 @@
 typedef struct {
     int screen_width;
     int screen_height;
+    
     void (*clear)(uint32_t color);
     void (*put_pixel)(int x, int y, uint32_t color);
+    
+    void (*draw_rect)(int x, int y, int w, int h, uint32_t color);
+    void (*draw_text)(int x, int y, const char* text, uint32_t color);
+    
     void (*update_display)(void);
-    void (*log)(const char* str);
+    void (*log)(const char* msg);
 } DE_API;
--- kernel/commands/cmd_kde.c
+++ kernel/commands/cmd_kde.c
@@ -6,11 +6,27 @@
 #include <lib/commands.h>
 #include <lib/string.h>
 #include <kernel/printk.h>
+#include <kernel/drivers/video/gfx.h>
+
+// Dikdörtgen çizim köprüsü
+void kernel_draw_rect(int start_x, int start_y, int w, int h, uint32_t color) {
+    for (int y = start_y; y < start_y + h; y++) {
+        for (int x = start_x; x < start_x + w; x++) {
+            fb_putpixel(x, y, color);
+        }
+    }
+}
+
+// Hatayı çözen yazı çizim köprüsü (Wrapper)
+// DE_API'den (x, y, text, color) alır, kernel'a (x, y, color, text) olarak iletir.
+void kernel_draw_text(int x, int y, const char* text, uint32_t color) {
+    gfx_draw_text_utf8(x, y, color, text);
+}
 
 void cmd_kde(int argc, char** argv) {
     (void)argc; (void)argv;
 
-    commands_puts("[KDE LOADER V2] /home/anil/desktop.kde yukleniyor...\n");
+    commands_puts("[KDE LOADER V2] /sys/de/desktop.kde yukleniyor...\n");
 
     uint32_t max_size = 64 * 1024; 
     uint8_t* kde_buffer = (uint8_t*)kmalloc(max_size);
@@ -22,14 +38,14 @@ void cmd_kde(int argc, char** argv) {
 
     uint32_t nread = 0;
 
-    if (vfs_read_all("/home/anil/desktop.kde", kde_buffer, max_size, &nread)) {
+    if (vfs_read_all("/sys/de/desktop.kde", kde_buffer, max_size, &nread)) {
         if (nread == 0) {
-            commands_puts("Error: /home/anil/desktop.kde is empty!\n");
+            commands_puts("Error: /sys/de/desktop.kde is empty!\n");
             kfree(kde_buffer);
             return;
         }
     } else {
-        commands_puts("Error: Could not read /home/anil/desktop.kde. Make sure the file exists.\n");
+        commands_puts("Error: Could not read /sys/de/desktop.kde. Make sure the file exists.\n");
         kfree(kde_buffer);
         return;
     }
@@ -42,6 +58,10 @@ void cmd_kde(int argc, char** argv) {
     api.screen_height  = height;
     api.clear          = fb_clear;
     api.put_pixel      = fb_putpixel;
+
+    api.draw_rect      = kernel_draw_rect;
+    api.draw_text      = kernel_draw_text; 
+
     api.update_display = fb_present;
     api.log            = (void(*)(const char*))printk;
 
