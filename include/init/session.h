890b3b030f7a6d6a150c74265d96c73bdaa0766c
Anıl <erdenanil900@gmail.com>
2026-08-23 00:27:37 +0300

    feat: implement interactive user session and login system

--- include/init/session.h
+++ include/init/session.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
+typedef struct {
+    uint32_t uid;
+    uint32_t gid;
+    char username[32];
+    char home_dir[128];
+} user_session_t;
+
 void session_init(void);
+user_session_t* session_get_current(void);
+void session_set_user(uint32_t uid, const char* username, const char* home);
 void session_handle_scancode(uint16_t scancode);
-void session_tick(void); // Yeni eklenen frame/render kontrolü
 
 #endif
--- init/init.c
+++ init/init.c
@@ -4,5 +4,5 @@
 
 void os_init(void) {
     printk("[KuvixOS] Sistem servisleri ve TTY oturumu baslatiliyor...\n");
-    shell_init(); 
+    // shell_init(); 
 }
